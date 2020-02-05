f55c65a51f16b2194f7fdac5d1b97e4612fcbf43
Michael Wang <michaelw0395@gmail.com>
2020-02-05 21:09:47 +1100

    created about me page and made it responsive

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects, :path => "portfolio", only: [:index]
+  get '/about', to: 'pages#about', as: 'about'
 end

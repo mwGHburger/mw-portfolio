56dfedcd6efde4a72b278cf5dd17774ffc4df1a5
Michael Wang <michaelw0395@gmail.com>
2020-02-07 23:47:31 +1100

    added digital clock to homepage

--- app/javascript/packs/controller/index.js
+++ app/javascript/packs/controller/index.js
@@ -37,6 +37,11 @@ const homePageLoad = () => {
         card.classList.add("home-page-load");
       }, i * 180);
     });
+    const clock = document.querySelector(".current-clock");
+    setTimeout(() => {
+      clock.classList.add("current-clock-load");
+    }, 200 * document.querySelectorAll(".home-card").length);
+    console.log(clock);
   }
 };
 
@@ -82,4 +87,43 @@ const aboutPageLoad = () => {
   }
 };
 
-const backBtn = document.querySelector(".back-btn");
+const getCurrentTime = () => {
+  if (document.querySelector(".current-clock")) {
+    const clock = document.querySelector(".current-clock");
+    // clear the div
+    clock.innerHTML = "";
+    const date = new Date();
+    let amPm = "AM";
+    console.log(amPm);
+    let hour = date.getHours();
+    if (hour === 12) {
+      amPm = "PM";
+    } else if (hour > 12 && hour < 24) {
+      hour = hour - 12;
+      amPm = "PM";
+    } else if (hour === 24) {
+      hour = 0;
+    }
+    let minute = date.getMinutes();
+    // format
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+
+    clock.insertAdjacentHTML(
+      "beforeend",
+      `<h1>${hour}<span class="flicker">:</span>${minute} ${amPm}</h1>`
+    );
+
+    const flicker = document.querySelector(".flicker");
+    setTimeout(() => {
+      flicker.classList.add("active");
+    }, 1500);
+  }
+};
+getCurrentTime();
+setInterval(getCurrentTime, 2000);
